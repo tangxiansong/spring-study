5bf5ef054f1ed74faca3cfa4cef5d889acaf2eeb
tangxiansong <1526198692@qq.com>
2019-03-11 23:36:16 +0800

    orm

---
+++ orm/src/main/java/com/spring/orm/App.java
@@ -0,0 +1,12 @@
+package com.spring.orm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
